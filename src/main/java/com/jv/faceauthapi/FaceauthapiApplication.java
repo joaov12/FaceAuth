b5d4ed01c0b76d5ddad1425a0e55b04f30558f99
João Vitor Soares <jvfs12th@gmail.com>
2024-07-21 14:14:40 -0300

    init: Inicia projeto

---
+++ src/main/java/com/jv/faceauthapi/FaceauthapiApplication.java
@@ -0,0 +1,13 @@
+package com.jv.faceauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceauthapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceauthapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jv/faceauthapi/FaceauthapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jv.faceauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceauthapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
