8743ffc619ee55dd1fc27947cc7005fa1e2f5256
João Vitor Soares <jvfs12th@gmail.com>
2024-07-23 16:05:06 -0300

    build: Cria o client do Amazon S3

---
+++ src/main/java/com/jv/faceauthapi/config/AwsS3Config.java
@@ -0,0 +1,21 @@
+package com.jv.faceauthapi.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsS3Config {
+    
+    @Bean
+    public AmazonS3 s3client(){
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+}
