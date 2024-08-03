01e6982de689c7d00515aed6657cecabd17858d5
João Vitor Soares <jvfs12th@gmail.com>
2024-08-02 22:05:54 -0300

    feat: Adiciona metodo de verificação facial

--- src/main/java/com/jv/faceauthapi/service/FaceAuthService.java
+++ src/main/java/com/jv/faceauthapi/service/FaceAuthService.java
@@ -3,7 +3,12 @@ package com.jv.faceauthapi.service;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.model.*;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.jv.faceauthapi.dto.ResponseUserDTO;
+import com.jv.faceauthapi.exception.FaceNotRegisteredException;
+import com.jv.faceauthapi.exception.MultipleFacesDetectedException;
+import com.jv.faceauthapi.exception.NoFaceDetectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,7 +17,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.util.Comparator;
 import java.util.Objects;
+import java.util.Date;
 
 @Service
 public class FaceAuthService {
@@ -29,7 +36,7 @@ public class FaceAuthService {
 
     public void savePhotoinS3(MultipartFile photo) throws Exception {
         if(!isFace(photo.getBytes())){
-            throw new Exception("NÃO FOI IDENTIFICADO NENHUM ROSTO!");
+            throw new NoFaceDetectedException();
         }
 
         uploadToBucket(photo, false);
@@ -75,11 +82,40 @@ public class FaceAuthService {
         }
 
         if(result.getFaceDetails().size() > 1){
-            throw new Exception("MAIS DE UM ROSTO FOI IDENTIFICADO NA FOTO ENVIADA");
+            throw new MultipleFacesDetectedException();
         }
 
         return result.getFaceDetails().getFirst().getConfidence() > 85;
     }
 
+    public ResponseUserDTO getAuthenticationByFace(MultipartFile photo) throws Exception {
+        if(!isFace(photo.getBytes())){
+            throw new NoFaceDetectedException();
+        }
 
+        uploadToBucket(photo, true);
+
+        SearchFacesByImageResult result = rekognitionclient.searchFacesByImage(new SearchFacesByImageRequest()
+                .withCollectionId(COLLECTION_ID)
+                .withImage(new Image()
+                        .withS3Object(new com.amazonaws.services.rekognition.model.S3Object()
+                                .withBucket(BUCKET_NAME)
+                                .withName(photo.getOriginalFilename() + "temp")))
+                .withFaceMatchThreshold(similiarity));
+
+        s3client.deleteObject(new DeleteObjectRequest(BUCKET_NAME, photo.getOriginalFilename() + "temp"));
+
+        if (result.getFaceMatches().isEmpty()) {
+            throw new FaceNotRegisteredException();
+        }
+
+        FaceMatch matchedFace = result.getFaceMatches().stream()
+                .max(Comparator.comparing(f -> f.getFace().getConfidence()))
+                .orElseThrow(Exception::new);
+
+        return new ResponseUserDTO.Builder()
+                .userName(matchedFace.getFace().getExternalImageId())
+                .percentageSimilarity(matchedFace.getFace().getConfidence())
+                .build();
+    }
 }
